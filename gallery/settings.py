a24c6728205b791ef89c84450f9e6943a499157f
RonaldKiprotich <protich12@gmail.com>
2020-11-15 18:17:11 +0300

    cloudinary configurations

--- gallery/settings.py
+++ gallery/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path,os
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary',
     'bootstrap3',
     'photos.apps.PhotosConfig',
     'django.contrib.admin',
@@ -132,4 +137,12 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+
+cloudinary.config( 
+  cloud_name = "dcqlinvpj", 
+  api_key = "823699933136938", 
+  api_secret = "Rq9lVViYC7C9pCZNDda2shb91Zo" 
+)
 
