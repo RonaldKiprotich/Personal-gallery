645ae735aaf9ebbaf6b7b0e02553598414b5a484
RonaldKiprotich <protich12@gmail.com>
2020-11-16 00:35:09 +0300

    category urls

--- photos/urls.py
+++ photos/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-   url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^gallery$', views.gallery, name='gallery'),
     url(r'^search/', views.search_category, name = 'search_category'),
+    url('location/(?P<location>\w+)',views.getLocations,name = 'location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
