2b1c7dadc3fb44264b177712ea6166b81834effc
RonaldKiprotich <protich12@gmail.com>
2020-11-15 19:53:07 +0300

    gallery url

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    path('',views.home,name = 'home'),
+   url(r'^$', views.home, name='home'),
+    url(r'^gallery$', views.gallery, name='gallery'),
+    url(r'^search/', views.search_category, name = 'search_category'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
